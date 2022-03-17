009bd941e3876665f950e974c48692c0748be0c1
faisalibal33 <faisaliqbal030397@gmail.com>
2022-03-17 19:09:20 +0700

    final

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import './Contact.css';
 import {AiOutlineMail} from 'react-icons/ai';
 import {BsMessenger, BsWhatsapp} from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rcmev9s', 'template_50zdiuv', form.current, 'PMh5gdsylcmjoRX0e')
+    
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get in touch</h5>
@@ -30,10 +42,10 @@ const Contact = () => {
             <a href="https://api.whatsapp.com/send?phone=+62859106712867" rel="noreferrer" target='_blank'>Send a messages</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
-          <textarea name="message" rows="7" placeholder='Your Messages' required></textarea>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
           <button type='submit' className='btn btn-primary'>Submit</button>
         </form>
       </div>
--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import './Footer.css';
+import {BsFacebook, BsInstagram, BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>Faisal</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portofolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_social">
+        <a href="https://web.facebook.com/swaeipers/"><BsFacebook/></a>
+        <a href="https://www.instagram.com/faisalibal/"><BsInstagram/></a>
+        <a href="https://twitter.com/faisalibal"><BsTwitter/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy;Faisal</small>
+      </div>
+    </footer>
   )
 }
 
